66ca3329ec3686e8eace59ab60d51c9e4a521fe8
Salim Jama <eaasjam@students.eaaa.dk>
2024-04-30 10:09:15 +0200

    alt lol

---
+++ Controllers/SpeakerController.cs
@@ -0,0 +1,31 @@
+﻿using Moq;
+using Microsoft.AspNetCore.Mvc;
+using SpeakerService.Interfaces;
+
+namespace MSpeakerMeeting.API.Controllers
+{
+    public class SpeakerController
+    {
+    }
+}
+
+namespace MSpeakerMeeting.API
+{
+    [Route("api/[controller]")]
+    public class SpeakerController:Controller
+    {
+        Mock<ISpeakerService> _speakerServiceMock;
+
+        public SpeakerController(Mock<ISpeakerService> speakerServiceMock)
+        {
+            _speakerServiceMock=speakerServiceMock;
+        }
+
+        public IActionResult GetAll()
+        {
+            _speakerServiceMock.Object.GetSummaries();
+            return Ok(new List<SpeakerSummary>());
+        }
+
+    }
+}
